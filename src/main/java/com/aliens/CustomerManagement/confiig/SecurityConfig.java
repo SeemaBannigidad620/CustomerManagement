747f390fea264f4bfe9187544db045be51af7d4c
SeemaBannigidad620 <seemabannigidad@gmail.com>
2024-01-12 15:26:53 +0530

    refactored

--- src/main/java/com/aliens/CustomerManagement/confiig/SecurityConfig.java
+++ src/main/java/com/aliens/CustomerManagement/confiig/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.aliens.CustomerManagement.congif;
+package com.aliens.CustomerManagement.confiig;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
